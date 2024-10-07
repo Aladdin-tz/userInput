c94e59649ae675a2aa7555bfeaa2a94aaf789aa9
allenlyimo <alenlyimo@hmail.com>
2024-10-07 22:23:12 +0300

    Modifying the comment line

--- user_input.py
+++ user_input.py
@@ -3,5 +3,5 @@ name = input('What is your name:')
 age = input('What is your age:')
 location = input('What is your location:')
 
-# Display Personalized message
+# Displaying Personalized message
 print(f'Hello {name}, you are {age} years old and live in {location}')
