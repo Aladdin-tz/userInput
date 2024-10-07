fa72628198b3b7be29825f9baea9ea3cb36d48d1
allenlyimo <alenlyimo@hmail.com>
2024-10-07 22:18:41 +0300

    First Commit

--- user_input.py
+++ user_input.py
@@ -3,5 +3,5 @@ name = input('What is your name:')
 age = input('What is your age:')
 location = input('What is your location:')
 
-# Dipsplay Personalized message
+# Display Personalized message
 print(f'Hello {name}, you are {age} years old and live in {location}')
